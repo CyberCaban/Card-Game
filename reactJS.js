0eaf263b8564c6c8f8f6f5a097fbc3134686e5c3
CyberCaban <74456694+CyberCaban@users.noreply.github.com>
2022-06-02 01:43:08 +0300

    Add files via upload

--- reactJS.js
+++ reactJS.js
@@ -1,6 +1,73 @@
-class createCardBtn extends React.Component{
+class CreateCardBtn extends React.Component{
     constructor(props){
         super(props)
+        this.state = {
+            archive:[],
+            cardTypes:["Magician", "Hermit"],
+            value: "",
+            typeValue: "Magician"
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.typeChange = this.typeChange.bind(this);
+        this.addToArchive = this.addToArchive.bind(this);
+    }
+
+    addToArchive(type, title){
+        const arr = this.state.archive
+        arr.push({
+            cardType: type,
+            cardTitle: title
+        })
+        this.setState({archive: arr})
+        console.log(this.state.archive);
+    }
+
+    // deleteFromArchive(){
+    //     console.log(2);
+    // }
+
+    handleChange(event){
+        this.setState({value: event.target.value})
+        console.log(this.state.value);
+    }
+
+    typeChange(event){
+        this.setState({
+            typeValue: event.target.value
+        })
+        return typeValue
+    }
+
+    createCard(){
+        switch (this.state.typeValue) {
+            case "Magician":
+                
+                break;
+            case "Hermit":
+                
+                break;
+            default:
+                break;
+        }
+    }
+
+    render(){
+        const typeMap = this.state.cardTypes.map((type)=>{
+            return <option value={type}>{type}</option>
+        })
+        // const existingCards = this.state.archive.cardTitle
+        const titleInput = this.state.value
+        const typeInput = this.state.typeValue
+        // console.log(typeMap);
+        return <div className="createCardBox">
+            <button onClick={()=>this.addToArchive(typeInput, titleInput)} className="createCardBtn">Create Card</button>
+            <input onChange={this.handleChange} className="cardNameInput" type="text"></input>
+            <select onChange={this.typeChange} name="cardType" id="createcardType">{typeMap}</select>
+            <div className="deleteCard">
+                <button className="deleteCardBtn">Delete Card</button>
+                <select name="deleteCardType" id="deleteCardType"></select>
+            </div>
+        </div>
     }
 }
 
@@ -12,8 +79,7 @@ class ColCard extends React.Component{
         this.cardname = props.name
         this.state = {
             isDragging: false,
-            initialPos: {left: 1, top: 1},
-            archive:[]
+            initialPos: {left: 0, top: 0}
         }
     }
 
@@ -49,7 +115,7 @@ class ColCard extends React.Component{
 
     createCard(){
         return <div className={`cardholder cardholder-${this.cardname}`}>
-            <div style={this.state.initialPos} className={`card card-${this.cardname}`} onMouseDown={this.onDragStart} onMouseMove={this.onDrag} onMouseUp={this.onDragStop}>
+            <div style={this.state.initialPos} className={`card Mg card-${this.cardname}`} onMouseDown={this.onDragStart} onMouseMove={this.onDrag} onMouseUp={this.onDragStop}>
                 <h1 unselectable="on">{this.cardname}</h1>
             </div>
         </div>
@@ -60,8 +126,18 @@ class ColCard extends React.Component{
     }
 }
 
-
+class FatCard extends ColCard{
+    createCard(){
+        return <div className={`cardholder cardholder-${this.cardname}`}>
+            <div style={this.state.initialPos} className={`card Hm card-${this.cardname}`} onMouseDown={this.onDragStart} onMouseMove={this.onDrag} onMouseUp={this.onDragStop}>
+                <h1 unselectable="on">{this.cardname}</h1>
+            </div>
+        </div>
+    }
+}
 
 
 const root = ReactDOM.createRoot(document.getElementById('cards'));
-root.render(<ColCard name="s" />);
+// root.render(<ColCard name="s" />);
+// root.render(<FatCard name="s" />);
+root.render(<CreateCardBtn name="s" />);
