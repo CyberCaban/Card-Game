ee1f45c0d53d90c30761d32b9112bcf6a8fcaba1
Dmitry <andrewgamer63@gmail.com>
2022-06-24 14:23:12 +0300

    attack button

--- reactJS.js
+++ reactJS.js
@@ -2,10 +2,12 @@ class CollisionElements extends React.Component{
     constructor(props){
         super(props)
         this.place1Ref = React.createRef()
+        this.opp1Ref = React.createRef()
         this.place2Ref = React.createRef()
+        this.opp2Ref = React.createRef()
         this.place3Ref = React.createRef()
+        this.opp3Ref = React.createRef()
         this.state = {
-            isDraggingCard:false,
             place1occ:'',
             place2occ:'',
             place3occ:'',
@@ -13,24 +15,8 @@ class CollisionElements extends React.Component{
             opponent:false
         }
     }
-
-    // collision(e){
-    //     console.log(e.target.parentElement.parentElement.children[1].children[0].children[0].children); //массив кардхолдеров
-    //     // console.log(e.target.parentElement.parentElement.children[1].children[0].children[0].children[0].children[0].getBoundingClientRect()); //обращение к конкретной карточке
-    //                 // e.target.parentElement.parentElement.children[1].children[0].children[0].children.map(cardholder=>{
-    //                 //     console.log(cardholder);
-    //                 // })
-    //     for (let i = 0; i < e.target.parentElement.parentElement.children[1].children[0].children[0].children.length; i++) {
-    //         console.log(e.target.parentElement.parentElement.children[1].children[0].children[0].children[i]);
-    //     }
-    //     // if (isDraggingCard === true) {
-            
-    //     // }
-    //     // React.Children.map(children=>{console.log(children);})
-    // }
     
-    updateData = (value, occupancy, cardName) => {
-        this.setState({isDraggingCard: value})
+    updateData = (occupancy, cardName) => {
         if (occupancy === "num1") {
             this.setState({place1occ:cardName})
         }if (occupancy === "num2") {
@@ -43,6 +29,7 @@ class CollisionElements extends React.Component{
         console.log(this.state.place1occ);
         console.log(this.state.place2occ);
         console.log(this.state.place3occ);
+        console.log(" ");
     }
 
     opponent = () => {
@@ -56,20 +43,31 @@ class CollisionElements extends React.Component{
             <div ref={this.place2Ref} className="place num2"><p unselectable="on">2</p></div>
             <div ref={this.place3Ref} className="place num3"><p unselectable="on">3</p></div>
         </div>
+        const opponentField = <div id="opponentField">
+            <div ref={this.opp3Ref} className="place num3"><p unselectable="on">3</p></div>
+            <div ref={this.opp2Ref} className="place num2"><p unselectable="on">2</p></div>
+            <div ref={this.opp1Ref} className="place num1"><p unselectable="on">1</p></div>
+        </div>
         return <div id="dragArea">
             <div id="up">
-                <div className="item_1">1</div>
-                <div className="info">
-                    <ul><strong>Rules</strong>
-                        <li>You cant use space while naming your card</li>
-                        <li>You cant create cards with the same names</li>
-                    </ul>
+                <div className="navbar">
+                    <div className="item_1">1</div>
+                    <div className="info">
+                        <ul><strong>Rules</strong>
+                            <li>You cant use space while naming your card</li>
+                            <li>You cant create cards with the same names</li>
+                        </ul>
+                    </div>
+                    <div className="item_2" onClick={this.opponent}>2</div>
                 </div>
-                <div className="item_2" onClick={this.opponent}>2</div>
+                <div className="wrapper">
+                    <div className="filler"></div>
+                    {this.state.opponent?opponentField:null}
+                </div>
             </div>
             <div id="down">
-                {Places}
                 <CreateCardBtn updateData={this.updateData} placeLoc={[this.place1Ref,this.place2Ref,this.place3Ref]} placeOccup={[this.state.place1occ,this.state.place2occ,this.state.place3occ]}/>
+                {Places}
             </div>
         </div>
     }
@@ -105,7 +103,7 @@ class CreateCardBtn extends React.Component{
 
     addToArchive(type, title, atk, def){
         if (this.state.value != "" && !this.state.value.includes(" ")) {
-            if (this.state.archive.find(card => card.cardTitle) === undefined) {
+            if (this.state.archive.find(card => card.cardTitle) == undefined && !this.state.archive) {
                 return alert('Check the rules!')
             }
             this.setState({value:" "})
@@ -144,13 +142,13 @@ class CreateCardBtn extends React.Component{
 
     handleAtkChange(event){
         this.setState({
-            atkValue: event.target.value*100
+            atkValue: event.target.value
         })
     }
 
     handleDefChange(event){
         this.setState({
-            defValue: event.target.value*100
+            defValue: event.target.value
         })
     }
 
@@ -169,6 +167,7 @@ class CreateCardBtn extends React.Component{
         })
         const creation = this.state.archive.map((card, index)=>{
             if (card.cardType === "Magician") {
+                // return <ResizeHandler name={card.cardTitle} atk={card.atk} def={card.def} key={card.cardTitle} updateData={this.props.updateData} placeLoc={this.props.placeLoc} placeOccup={this.props.placeOccup} cardsInHand={index*20}/> 
                 return <ColCard name={card.cardTitle} atk={card.atk} def={card.def} key={card.cardTitle} updateData={this.props.updateData} placeLoc={this.props.placeLoc} placeOccup={this.props.placeOccup} cardsInHand={index*20}/>
             }
             if (card.cardType === "Hermit") {
@@ -184,14 +183,14 @@ class CreateCardBtn extends React.Component{
                 {creation}
             </div>
             <div className="createCardBox">
-                <button onClick={()=>this.addToArchive(typeInput, titleInput, atkInput)} className="createCardBtn">Create Card</button>
-                Name the card
+                <button onClick={()=>this.addToArchive(typeInput, titleInput, atkInput, defInput)} className="createCardBtn">Create Card</button>
+                <p className="CCBText">Name The Card</p>
                 <input onChange={this.handleChange} className="props.nameInput" type="text" placeholder={this.state.value} autoFocus></input>
-                ATKPower
+                <p className="CCBText">ATKPower</p>
                 <input onChange={this.handleAtkChange} type="number"></input>
-                DEFPower
+                <p className="CCBText">DEFPower</p>
                 <input onChange={this.handleDefChange} type="number"></input>
-                Card Type
+                <p className="CCBText">Card Type</p>
                 <select onChange={this.typeChange} name="cardType" id="createcardType">{typeMap}</select>
                 <div className="deleteCard">
                     <button onClick={this.deleteFromArchive} className="deleteCardBtn">Delete Card</button>
@@ -208,53 +207,30 @@ class ColCard extends React.Component{
         this.x
         this.y
         this.state = {
-            isDragging: false,
-            initialPos: {left: (window.innerWidth / 2)+(this.props.cardsInHand), top: window.innerHeight - 250},
+            initialPos: {left: (window.innerWidth * 0.15)+(this.props.cardsInHand), top: window.innerHeight - 250},
             placeSelector: false,
             place: '',
             canAtk: false,
-        }
-    }
-
-    onDragStart = (event) => {
-        if (event.type === "mousedown") {
-            this.setState({isDragging: true})
+            placeAttackSelector:false,
         }
     }
 
     onDrag = (event) => {
-        if (this.state.isDragging === true) {
-            // this.setState({
-            //     initialPos:{
-            //         left: event.clientX - 66,
-            //         top: event.clientY - 121
-            //     }
-            // })
-            if (this.state.place === "num1") {
-                this.setState({initialPos:{left: this.props.placeLoc[0].current.getBoundingClientRect().x, top: this.props.placeLoc[0].current.getBoundingClientRect().y}})
-            }if (this.state.place === "num2") {
-                this.setState({initialPos:{left: this.props.placeLoc[1].current.getBoundingClientRect().x, top: this.props.placeLoc[1].current.getBoundingClientRect().y}})
-            }if (this.state.place === "num3") {
-                this.setState({initialPos:{left: this.props.placeLoc[2].current.getBoundingClientRect().x, top: this.props.placeLoc[2].current.getBoundingClientRect().y}})
+        this.props.updateData(this.state.place, this.props.name)
+        this.setState({
+            initialPos:{
+                left: (window.innerWidth * 0.15)+(this.props.cardsInHand),
+                top: window.innerHeight - 250
             }
-            this.props.updateData(this.state.isDragging, this.state.place, this.props.name)
+        })
+        if (this.state.place === "num1") {
+            this.setState({initialPos:{left: this.props.placeLoc[0].current.getBoundingClientRect().x, top: this.props.placeLoc[0].current.getBoundingClientRect().y}})
+        }if (this.state.place === "num2") {
+            this.setState({initialPos:{left: this.props.placeLoc[1].current.getBoundingClientRect().x, top: this.props.placeLoc[1].current.getBoundingClientRect().y}})
+        }if (this.state.place === "num3") {
+            this.setState({initialPos:{left: this.props.placeLoc[2].current.getBoundingClientRect().x, top: this.props.placeLoc[2].current.getBoundingClientRect().y}})
         }
     }
-
-    // onDragStop = (event) => {
-    //     if (event.type === "mouseup") {
-    //         if (this.state.isDragging === true) {
-    //             this.setState({isDragging: false})
-    //             if (this.state.place === "num1") {
-    //                 this.setState({initialPos:{left: this.props.placeLoc[0].current.getBoundingClientRect().x, top: this.props.placeLoc[0].current.getBoundingClientRect().y}})
-    //             }if (this.state.place === "num2") {
-    //                 this.setState({initialPos:{left: this.props.placeLoc[1].current.getBoundingClientRect().x, top: this.props.placeLoc[1].current.getBoundingClientRect().y}})
-    //             }if (this.state.place === "num3") {
-    //                 this.setState({initialPos:{left: this.props.placeLoc[2].current.getBoundingClientRect().x, top: this.props.placeLoc[2].current.getBoundingClientRect().y}})
-    //             }
-    //         }
-    //     }
-    // }
 
     CardPlaceSelect = (event) => {
         if (this.state.placeSelector === false) {
@@ -262,63 +238,77 @@ class ColCard extends React.Component{
         }else{
             this.setState({placeSelector: false})
         }
+        // this.setState({placeAttackSelector:false})
     }
 
     btnPlaceSelect = (place) => {
-        if (place === "1") {
+        if (place === "move1") {
             if (this.props.placeOccup[0] === this.props.name) {
                 return
             }if (this.props.placeOccup[0] != '') {
                 return alert("Place is occupied")
             }
             this.setState({initialPos:{left: this.props.placeLoc[0].current.getBoundingClientRect().x, top: this.props.placeLoc[0].current.getBoundingClientRect().y}})
-            this.props.updateData(null, this.state.place, '')
+            this.props.updateData(this.state.place, '')
             this.setState({place: "num1", canAtk:true})
         }
-        if (place === "2") {
+        if (place === "move2") {
             if (this.props.placeOccup[1] === this.props.name) {
                 return
             }if (this.props.placeOccup[1] != '') {
                 return alert("Place is occupied")
             }
             this.setState({initialPos:{left: this.props.placeLoc[1].current.getBoundingClientRect().x, top: this.props.placeLoc[1].current.getBoundingClientRect().y}})
-            this.props.updateData(null, this.state.place, '')
+            this.props.updateData(this.state.place, '')
             this.setState({place: "num2", canAtk:true})
         }
-        if (place === "3") {
+        if (place === "move3") {
             if (this.props.placeOccup[2] === this.props.name) {
                 return
             }if (this.props.placeOccup[2] != '') {
                 return alert("Place is occupied")
             }
             this.setState({initialPos:{left: this.props.placeLoc[2].current.getBoundingClientRect().x, top: this.props.placeLoc[2].current.getBoundingClientRect().y}})
-            this.props.updateData(null, this.state.place, '')
+            this.props.updateData(this.state.place, '')
             this.setState({place: "num3", canAtk:true})
         } if (place === "Hand") {
-            this.setState({initialPos:{left: (window.innerWidth / 2)+(this.props.cardsInHand), top: window.innerHeight - 250}})
-            this.props.updateData(null, this.state.place, '')
+            this.setState({initialPos:{left: (window.innerWidth * 0.15)+(this.props.cardsInHand), top: window.innerHeight - 250}})
+            this.props.updateData(this.state.place, '')
             this.setState({place: "Hand", canAtk:false})
+        }if (place === "Attack") {
+            this.setState({placeAttackSelector:true})
+        }if (place === "Back") {
+            this.setState({placeAttackSelector:false})
         }
     }
 
     render(){
         const chooseWindow = <div className="chooseWindow">
-            <button onClick={()=>this.btnPlaceSelect("1")}>1</button>
-            <button onClick={()=>this.btnPlaceSelect("2")}>2</button>
-            <button onClick={()=>this.btnPlaceSelect("3")}>3</button>
+            <button onClick={()=>this.btnPlaceSelect("move1")}>1</button>
+            <button onClick={()=>this.btnPlaceSelect("move2")}>2</button>
+            <button onClick={()=>this.btnPlaceSelect("move3")}>3</button>
             {this.state.canAtk?<button onClick={()=>this.btnPlaceSelect("Attack")}>Attack</button>:null}
             <button onClick={()=>this.btnPlaceSelect("Hand")}>Hand</button>
+        </div>
+        const attackWindow = <div className="attackWindow">
+            <button onClick={()=>this.btnPlaceSelect("atk1")}>Attack 1</button>
+            <button onClick={()=>this.btnPlaceSelect("atk2")}>Attack 2</button>
+            <button onClick={()=>this.btnPlaceSelect("atk3")}>Attack 3</button>
+            <button onClick={()=>this.btnPlaceSelect("Back")}>Back</button>
         </div>
         return <div className={`cardholder cardholder-${this.props.name}`}>
             <div style={this.state.initialPos} className={`card Mg card-${this.props.name} ${this.state.place} ${this.state.placeSelector?"active":""}`} onMouseDown={this.onDragStart} onMouseMove={this.onDrag} onMouseUp={this.onDragStop} onClick={this.CardPlaceSelect}>
                 <h2 unselectable="on">{this.props.name}</h2>
                 <img draggable="false" unselectable="on" src="/img/cardIMG0.png" alt="" />
-                <p unselectable="on">Description</p>
-                <div>
-                    <p className="atkPower">{this.props.atk}</p>
-                    <p className="defPower">{this.props.def}</p>
+                <div className="cardProperties">
+                    <p unselectable="on">Description</p>
+                    <div className="powers">
+                        <p className="atkPower">{this.props.atk}</p>
+                        <p className="defPower">{this.props.def}</p>
+                    </div>
                 </div>
                 {this.state.placeSelector?chooseWindow:null}
+                {this.state.placeAttackSelector?attackWindow:null}
             </div>
         </div>
     }
@@ -330,20 +320,50 @@ class FatCard extends ColCard{
             <button onClick={()=>this.btnPlaceSelect("1")}>1</button>
             <button onClick={()=>this.btnPlaceSelect("2")}>2</button>
             <button onClick={()=>this.btnPlaceSelect("3")}>3</button>
+            {this.state.canAtk?<button onClick={()=>this.btnPlaceSelect("Attack")}>Attack</button>:null}
             <button onClick={()=>this.btnPlaceSelect("Hand")}>Hand</button>
         </div>
         return <div className={`cardholder cardholder-${this.props.name}`}>
             <div style={this.state.initialPos} className={`card Hm card-${this.props.name} ${this.state.place} ${this.state.placeSelector?"active":""}`} onMouseDown={this.onDragStart} onMouseMove={this.onDrag} onMouseUp={this.onDragStop} onClick={this.CardPlaceSelect}>
                 <h2 unselectable="on">{this.props.name}</h2>
                 <img draggable="false" unselectable="on" src="/img/cardIMG1.png" alt="" />
-                <p unselectable="on">Description</p>
-                <p className="aktPower">{this.props.atk}</p>
-                <p className="defPower">{this.props.def}</p>
+                <div className="cardProperties">
+                    <p unselectable="on">Description</p>
+                    <div className="powers">
+                        <p className="atkPower">{this.props.atk}</p>
+                        <p className="defPower">{this.props.def}</p>
+                    </div>
+                </div>
                 {this.state.placeSelector?chooseWindow:null}
             </div>
         </div>
     }
 }
+
+
+
+// function debounce(fn, ms) {
+//     let timer
+//     return _ => {
+//         clearTimeout(timer)
+//         timer = setTimeout(_ => {
+//             timer = null
+//             fn.apply(this, arguments)
+//         }, ms)
+//     };
+// }
+// function ResizeHandler(card) {
+//     React.useEffect(() => {
+//         const debouncedHandleResize = debounce(function handleResize() {
+//             console.log(card.name);
+//         }, 100)
+//         window.addEventListener('resize', debouncedHandleResize)
+//         return _ => {
+//             window.removeEventListener('resize', debouncedHandleResize)
+//         }
+//     })
+//     return <ColCard name={card.name} atk={card.atk} def={card.def} key={card.key} updateData={card.updateData} placeLoc={card.placeLoc} placeOccup={card.placeOccup} cardsInHand={card.cardsInHand}/>
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<CollisionElements />);
